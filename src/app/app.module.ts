b7dfb14cb9a5ddd425e07818b5133bf141dc7b81
Ho Tan Anh <anh152k3@gmail.com>
2024-01-10 22:58:04 +0700

    add component sum,qa,quizz,shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { NavLeftComponent } from './home/nav-left/nav-left.component';
-import { ChatResultComponent } from './home/chat-result/chat-result.component';
+import { NavLeftComponent } from './shared/nav-left/nav-left.component';
+import { ChatResultComponent } from './shared/chat-result/chat-result.component';
 import { FormsModule } from '@angular/forms';
-import { FeatureComponent } from './home/feature/feature.component';
+import { FeatureComponent } from './shared/feature/feature.component';
 import { MatMenuModule } from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
+import { SumComponent } from './sum/sum.component';
+import { SharedComponent } from './shared/shared.component';
+import { QuizizzComponent } from './quizizz/quizizz.component';
+import { QaComponent } from './qa/qa.component';
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavLeftComponent,
     ChatResultComponent,
-    FeatureComponent
+    FeatureComponent,
+    SumComponent,
+    SharedComponent,
+    QuizizzComponent,
+    QaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     MatMenuModule,
-    MatButtonModule
+    MatButtonModule,
+    RouterModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
